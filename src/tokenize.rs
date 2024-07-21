9992d78599e9e875457ace292a04eb4abfc33400
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-21 11:05:06 +0300

    Add assignment & equality operators to tokenizer

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,13 +1,65 @@
-use std::{ collections::HashMap, fs, io::{ self, Write } };
+use std::{ collections::HashMap, fs };
 
 pub fn tokenize(filename: &str) -> i32 {
     let mut result = 0;
 
     let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-        writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+        eprintln!("Failed to read file {}", filename);
         String::new()
     });
 
+    // Uncomment this block to pass the first stage
+    if !file_contents.is_empty() {
+        file_contents
+            .lines()
+            .enumerate()
+            .for_each(|(line_index, line)| {
+                if print_punctuators(line_index, line) {
+                    result = 65;
+                }
+            });
+        println!("EOF  null");
+    } else {
+        println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+    }
+
+    return result;
+}
+
+fn print_punctuators(index: usize, line: &str) -> bool {
+    let mut result = false;
+
+    let double_pairs = HashMap::from([
+        ("!=", "BANG_EQUAL"),
+        ("==", "EQUAL_EQUAL"),
+        ("<=", "LESS_EQUAL"),
+        (">=", "GREATER_EQUAL"),
+        ("!=", "BANG_EQUAL"),
+    ]);
+
+    let mut iterator = line.chars().into_iter().peekable();
+
+    while let Some(ch) = iterator.next() {
+        if let Some(next_ch) = iterator.peek() {
+            let dp = format!("{}{}", ch, next_ch);
+
+            if let Some(value) = double_pairs.get(dp.as_str()) {
+                println!("{value} {dp} null");
+                iterator.next();
+                continue;
+            }
+        }
+
+        if print_pair(&ch).is_err() {
+            eprintln!("[line {}] Error: Unexpected character: {ch}", index + 1);
+            result = true;
+        }
+    }
+
+    return result;
+}
+
+fn print_pair(ch: &char) -> Result<(), ()> {
     let pairs = HashMap::from([
         ('(', "LEFT_PAREN"),
         (')', "RIGHT_PAREN"),
@@ -18,45 +70,20 @@ pub fn tokenize(filename: &str) -> i32 {
         ('+', "PLUS"),
         ('-', "MINUS"),
         ('*', "STAR"),
-        ('!', "BANG_EQUAL"),
-        ('=', "EQUAL_EQUAL"),
-        ('<', "LESS_EQUAL"),
-        ('>', "GREATER_EQUAL"),
-        ('!', "BANG_EQUAL"),
         ('<', "LESS"),
         ('>', "GREATER"),
         ('/', "SLASH"),
         ('.', "DOT"),
+        ('=', "EQUAL"),
     ]);
 
-    // Uncomment this block to pass the first stage
-    if !file_contents.is_empty() {
-        file_contents
-            .lines()
-            .into_iter()
-            .enumerate()
-            .for_each(|(line_num, line)| {
-                line.chars()
-                    .into_iter()
-                    .for_each(|ch| {
-                        match pairs.get(&ch) {
-                            Some(val) => {
-                                println!("{val} {ch} null");
-                            }
-                            None => {
-                                result = 65;
-                                eprintln!(
-                                    "[line {}] Error: Unexpected character: {ch}",
-                                    line_num + 1
-                                )
-                            }
-                        }
-                    })
-            });
-        println!("EOF  null");
-    } else {
-        println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+    match pairs.get(&ch) {
+        Some(value) => {
+            println!("{value} {ch} null");
+            return Ok(());
+        }
+        None => {
+            return Err(());
+        }
     }
-    
-    return result;
 }
